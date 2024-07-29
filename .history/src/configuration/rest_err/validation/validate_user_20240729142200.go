620bd7ef31d816f4de3d2ba0e7379c3f63747200
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2024-07-29 14:26:52 -0300

    validation user request

---
+++ .history/src/configuration/rest_err/validation/validate_user_20240729142200.go
@@ -0,0 +1,35 @@
+package validation
+
+import (
+	"encoding/json"
+
+	"github.com/biduzilla/go-crud/src/configuration/rest_err"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	en_translation "github.com/go-playground/validator/v10/translations/en"
+)
+
+var (
+	Validate = validator.New()
+	transl   ut.Translator
+)
+
+func init() {
+	if val, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		en := en.New()
+		unt := ut.New(en, en)
+		transl, _ = unt.GetTranslator("en")
+		en_translation.RegisterDefaultTranslations(val, transl)
+	}
+}
+
+func ValidateUserError(err error) *rest_err.RestErr {
+	var jsonErr *json.UnmarshalTypeError
+	var jsonValidationError validator.ValidationErrors
+
+	if erros.As(err, jsonErr) {
+		return rest_err.NewBadRequestError("Invalid field type")
+	}
+}
